c64ff992586e4fe5359c3d8155be0c2425e233d6
Brian Yingling <brian@jet.com>
2019-03-01 23:20:39 -0500

    change

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
+            <h1>This is React</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
